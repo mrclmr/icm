36044efcd8d6f28df560653a80176e6de245ccea
Marcel Meyer <mm.marcelmeyer@gmail.com>
2025-04-12 14:43:57 +0200

    Remove redundant test names

--- cont/check_digit_test.go
+++ cont/check_digit_test.go
@@ -1,62 +1,54 @@
 package cont
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCalcCheckDigit(t *testing.T) {
-	type args struct {
+	tests := []struct {
 		ownerCode  string
 		equipCatID rune
 		serialNum  int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
+		want       int
 	}{
 		{
-			"Test ABC U 123456 0",
-			args{"ABC", 'U', 123456},
+			"ABC", 'U', 123456,
 			0,
 		},
 		{
-			"Test NYK U 008685 2",
-			args{"NYK", 'U', 8685},
+			"NYK", 'U', 8685,
 			2,
 		},
 		{
-			"Test NYK U 000000 0",
-			args{"NYK", 'U', 0},
+			"NYK", 'U', 0,
 			10,
 		},
 		{
-			"Test CMA U 163912 (1)0",
-			args{"CMA", 'U', 163912},
+			"CMA", 'U', 163912,
 			10,
 		},
 		{
-			"Test CMA U 169312 0",
-			args{"CMA", 'U', 169312},
+			"CMA", 'U', 169312,
 			0,
 		},
 		{
-			"Test CSQ U 305438 3",
-			args{"CSQ", 'U', 305438},
+			"CSQ", 'U', 305438,
 			3,
 		},
 		{
-			"Test CSQ U 999998 3",
-			args{"CSQ", 'U', 999998},
+			"CSQ", 'U', 999998,
 			3,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CalcCheckDigit(tt.args.ownerCode, tt.args.equipCatID, tt.args.serialNum); got != tt.want {
-				t.Errorf("CalcCheckDigit() = %v, want %v", got, tt.want)
-			}
-		})
+		t.Run(
+			fmt.Sprintf("%s %s %06d", tt.ownerCode, string(tt.equipCatID), tt.serialNum),
+			func(t *testing.T) {
+				if got := CalcCheckDigit(tt.ownerCode, tt.equipCatID, tt.serialNum); got != tt.want {
+					t.Errorf("CalcCheckDigit() = %v, want %v", got, tt.want)
+				}
+			})
 	}
 }
 
--- cont/transposition_test.go
+++ cont/transposition_test.go
@@ -1,65 +1,50 @@
 package cont
 
 import (
+	"fmt"
 	"slices"
 	"testing"
 )
 
 func TestCheckTransposition(t *testing.T) {
-	type args struct {
+	tests := []struct {
 		ownerCode  string
 		equipCatID rune
 		serialNum  int
 		checkDigit int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []TpNumber
+		want       []TpNumber
 	}{
 		{
-			name: "Test ABC U 123123 7",
-			args: args{
-				ownerCode:  "ABC",
-				equipCatID: 'U',
-				serialNum:  123123,
-				checkDigit: 7,
-			},
-			want: nil,
+			ownerCode:  "ABC",
+			equipCatID: 'U',
+			serialNum:  123123,
+			checkDigit: 7,
+			want:       nil,
 		},
 		{
-			name: "Test CMA U 163912 (1)0",
-			args: args{
-				ownerCode:  "CMA",
-				equipCatID: 'U',
-				serialNum:  163912,
-				checkDigit: 10,
-			},
+			ownerCode:  "CMA",
+			equipCatID: 'U',
+			serialNum:  163912,
+			checkDigit: 10,
 			want: []TpNumber{
 				{Number{"CMA", 'U', 169312, 0}, 2},
 				{Number{"CMA", 'U', 163192, 0}, 3},
 			},
 		},
 		{
-			name: "Test RCB U 001130 0",
-			args: args{
-				ownerCode:  "RCB",
-				equipCatID: 'U',
-				serialNum:  1130,
-				checkDigit: 0,
-			},
+			ownerCode:  "RCB",
+			equipCatID: 'U',
+			serialNum:  1130,
+			checkDigit: 0,
 			want: []TpNumber{
 				{Number{"RCB", 'U', 10130, 0}, 1},
 			},
 		},
 		{
-			name: "Test WSL U 801743 (1)0",
-			args: args{
-				ownerCode:  "WSL",
-				equipCatID: 'U',
-				serialNum:  801743,
-				checkDigit: 10,
-			},
+			ownerCode:  "WSL",
+			equipCatID: 'U',
+			serialNum:  801743,
+			checkDigit: 10,
 			want: []TpNumber{
 				{Number{"WSL", 'U', 810743, 0}, 1},
 				{Number{"WSL", 'U', 807143, 0}, 2},
@@ -67,13 +52,10 @@ func TestCheckTransposition(t *testing.T) {
 			},
 		},
 		{
-			name: "Test APL U 689473 (1)0",
-			args: args{
-				ownerCode:  "APL",
-				equipCatID: 'U',
-				serialNum:  689473,
-				checkDigit: 10,
-			},
+			ownerCode:  "APL",
+			equipCatID: 'U',
+			serialNum:  689473,
+			checkDigit: 10,
 			want: []TpNumber{
 				{Number{"APL", 'U', 869473, 0}, 0},
 				{Number{"APL", 'U', 698473, 0}, 1},
@@ -85,11 +67,13 @@ func TestCheckTransposition(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CheckTransposition(tt.args.ownerCode, tt.args.equipCatID, tt.args.serialNum, tt.args.checkDigit); !slices.Equal(got, tt.want) {
-				t.Errorf("CheckTransposition() = %v, want %v", got, tt.want)
-			}
-		})
+		t.Run(
+			fmt.Sprintf("%s %s %06d %d", tt.ownerCode, string(tt.equipCatID), tt.serialNum, tt.checkDigit),
+			func(t *testing.T) {
+				if got := CheckTransposition(tt.ownerCode, tt.equipCatID, tt.serialNum, tt.checkDigit); !slices.Equal(got, tt.want) {
+					t.Errorf("CheckTransposition() = %v, want %v", got, tt.want)
+				}
+			})
 	}
 }
 
